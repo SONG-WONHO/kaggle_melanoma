6d03f008bcbaf7e7ca0a109115d9224f84c1dd5a
sogna <win778500@gmail.com>
2020-07-23 12:35:41 +0900

    Update model.py

--- code/v15/model.py
+++ code/v15/model.py
@@ -80,6 +80,48 @@ class PoolModel(nn.Module):
         return outputs, outputs_sub1
 
 
+class HeadModel(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.model = EfficientNet.from_pretrained(config.backbone_name)
+        self.c = {
+            'efficientnet-b0': 1280,
+            'efficientnet-b1': 1280,
+            'efficientnet-b2': 1408,
+            'efficientnet-b3': 1536,
+            'efficientnet-b4': 1792,
+            'efficientnet-b5': 2048,
+            'efficientnet-b6': 2304,
+            'efficientnet-b7': 2560}[config.backbone_name]
+        self.dropout = nn.Dropout(config.dropout)
+        self.out = nn.Sequential(
+            nn.Linear(in_features=self.c, out_features=self.c, bias=True),
+            nn.LayerNorm(self.c),
+            nn.Dropout(0.2),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=self.c, out_features=config.num_targets, bias=True))
+        self.sub_1 = nn.Sequential(
+            nn.Linear(in_features=self.c, out_features=self.c, bias=True),
+            nn.LayerNorm(self.c),
+            nn.Dropout(0.2),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=self.c, out_features=3, bias=True))
+
+    def forward(self, x):
+        # features
+        feat = self.model.extract_features(x)
+        feat = F.avg_pool2d(feat, feat.size()[2:]).reshape(-1, self.c)
+
+        # original outputs
+        outputs = self.out(self.dropout(feat))
+
+        # sub outputs
+        outputs_sub1 = self.sub_1(self.dropout(feat))
+
+        return outputs, outputs_sub1
+
+
 def get_model(config):
     try:
         f = globals().get(f"{config.model_name}")
